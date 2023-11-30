069038f8f050bcf01ba639da7b5540d74d99c480
Jorge Luis Ayala Manrique <jorgelamanrique@gmail.com>
2023-11-30 04:39:46 -0600

    U3Front5

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -8,7 +8,7 @@ import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import Swal from 'sweetalert2';
 import { AuthService } from 'src/app/services/auth.service';
 import { ObserverServiceService } from 'src/app/services/observer-service.service';
-import { EMPTY } from 'rxjs';
+import { EMPTY, Observable, Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -25,6 +25,8 @@ export class InicioComponent implements OnInit {
   librosFiltrados: Libro[] = [];
   dialog: any;
   mostrar?: boolean;
+  private bookSubject = new Subject<string>();
+
 
   constructor(private _biblioService: BiblioService,
     private toastr: ToastrService,
@@ -40,25 +42,33 @@ export class InicioComponent implements OnInit {
       this.esAdmin = result;
       console.log(this.esAdmin);
     });
-
     // Suscribirse a cambios en los libros
-    this.observerService.getBookObservable().subscribe(bookId => {
-      console.log(`Se notificó un cambio en el libro con ID: ${bookId}`);
-      // Aquí puedes realizar acciones adicionales en respuesta al cambio en el libro
-    });
+
   }
 
-  notificarCambio(idLibro: string) {
+  notificarCambioDisp(idLibro: string) {
     // Realiza una solicitud al backend para notificar cambios
-    this.http.post(`http://localhost:9000/api/libros/${idLibro}/notificar-cambio`, {}).subscribe(
-        () => {
-            console.log('Notificación de cambio enviada con éxito');
-        },
-        error => {
-            console.error('Error al enviar notificación de cambio', error);
-        }
+    this.http.post(`http://localhost:9000/api/libros/${idLibro}/notificar-cambioDisp`, {}).subscribe(
+      () => {
+        console.log('Notificación de cambio enviada con éxito');
+      },
+      error => {
+        console.error('Error al enviar notificación de cambio', error);
+      }
     );
-}
+  }
+
+  notificarCambioPrest(idLibro: string) {
+    // Realiza una solicitud al backend para notificar cambios
+    this.http.post(`http://localhost:9000/api/libros/${idLibro}/notificar-cambioPrest`, {}).subscribe(
+      () => {
+        console.log('Notificación de cambio enviada con éxito');
+      },
+      error => {
+        console.error('Error al enviar notificación de cambio', error);
+      }
+    );
+  }
 
   filtrarLibros(event: any) {
     this.librosFiltrados = this.listLibros.filter(libro => libro.titulo.toLowerCase().includes(event.target.value.toLowerCase()));
@@ -94,28 +104,28 @@ export class InicioComponent implements OnInit {
           icon: "success"
         });
         // Obtener el ID del usuario actual desde tu servicio de autenticación
-      const usuarioId = this.authService.getUserId();
-      const userMail = this.authService.getUserMail();
+        const usuarioId = this.authService.getUserId();
+        const userMail = this.authService.getUserMail();
 
-      if (!usuarioId || !userMail) {
-        // Manejar el caso en el que usuarioId o userMail es null
-        console.warn('El usuario no tiene un ID o correo válido.');
-        return;
-      }
+        if (!usuarioId || !userMail) {
+          // Manejar el caso en el que usuarioId o userMail es null
+          console.warn('El usuario no tiene un ID o correo válido.');
+          return;
+        }
 
-      // Si tenemos el ID del usuario, intentamos suscribirlo al libro
-      if (usuarioId) {
-        
-        this.observerService.subscribeUserToBook(usuarioId,userMail, libroId).subscribe(
-          () => {
-            console.log(`Usuario ${usuarioId} con correo ${userMail} suscrito al libro ${libroId}`);
-          },
-          error => {
-            console.error("Error al suscribir al usuario al libro:", error);
-          }
-        );
-      }
-    },
+        // Si tenemos el ID del usuario, intentamos suscribirlo al libro
+        if (usuarioId) {
+
+          this.observerService.subscribeUserToBook(usuarioId, userMail, libroId).subscribe(
+            () => {
+              console.log(`Usuario ${usuarioId} con correo ${userMail} suscrito al libro ${libroId}`);
+            },
+            error => {
+              console.error("Error al suscribir al usuario al libro:", error);
+            }
+          );
+        }
+      },
       error => {
         Swal.fire({
           title: "Oh no!",
--- src/app/services/observer-service.service.ts
+++ src/app/services/observer-service.service.ts
@@ -17,8 +17,6 @@ export class ObserverServiceService {
     return this.bookSubject.asObservable();
   }
 
-  bookChanged$: Observable<string> = this.bookSubject.asObservable();
-
   subscribeUserToBook(userId: string, userMail: string, bookId: string): Observable<any> {
     const url = `http://localhost:9000/api/libros/${bookId}/aggUsr?usuarioId=${userId}&usuarioCorreo=${userMail}`; // Reemplaza con el valor correcto del correo
     
@@ -26,21 +24,6 @@ export class ObserverServiceService {
     const updatedSubscriptions = [...existingSubscriptions, userMail];
     this.bookSubscriptions.set(bookId, updatedSubscriptions);
     return this.http.post(url, {});
-
-    
-  }
-
-  // Método para notificar cambios en un libro
-  notifyBookChanged(bookId: string, userMails: any[]) {
-    this.bookSubject.next(bookId);
-    
-    // Notificar a usuarios específicos por correo electrónico
-    userMails.forEach(user => {
-      // Asegúrate de que user es un objeto antes de intentar acceder a sus propiedades
-      if (user && user.usuarioCorreo) {
-        console.log(`Notificar a ${user.usuarioCorreo} sobre el cambio en el libro ${bookId}`);
-      }
-    });
   }
   
   // Método para manejar la suscripción automática al agregar un favorito
